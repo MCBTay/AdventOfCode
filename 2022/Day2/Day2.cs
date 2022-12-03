ee469c8b035023adccd87b1f1ea8b5d4424d96d3
Bryan Taylor <mcbtay@gmail.com>
2022-12-03 00:21:47 -0500

    adding implementation to parse input

--- 2022/Day2/Day2.cs
+++ 2022/Day2/Day2.cs
@@ -5,6 +5,30 @@ public class Day2
 {
   public static void RockPaperScissors()
   {
+    var strategyGuide = ParseInput();
 
+    foreach (var game in strategyGuide) 
+    {
+      foreach (var move in game)
+      {
+        Console.Write(move + " ");
+      }
+      Console.WriteLine();
+    }
+  }
+
+  private static List<List<string>> ParseInput()
+  {
+    var games = new List<List<string>>();
+    
+    foreach (var line in System.IO.File.ReadLines(@"Day2/example_input.txt")) 
+    {
+      var split = line.Split(' ');
+      if (split.Count() < 2) continue;
+
+      games.Add(split.ToList()); 
+    }
+
+    return games;
   }
 }
