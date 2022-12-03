9d01e7750eb246ddabd969b6e535c999f3a4c420
Bryan Taylor <mcbtay@gmail.com>
2022-12-03 00:31:48 -0500

    implementing method to determine the points earned based on outcome of the game

--- 2022/Day2/Day2.cs
+++ 2022/Day2/Day2.cs
@@ -7,13 +7,56 @@ public class Day2
   {
     var strategyGuide = ParseInput();
 
+    var totalScore = 0;
     foreach (var game in strategyGuide) 
     {
-      foreach (var move in game)
-      {
-        Console.Write(move + " ");
-      }
-      Console.WriteLine();
+      totalScore += DetermineOutcome(game);
+    }
+
+    Console.WriteLine($"Total Score: {totalScore}");
+  }
+
+  private static int DetermineOutcome(List<string> game)
+  {
+    switch (game.First()) 
+    {
+      case "A": return OpponentThrewRock(game.Last());
+      case "B": return OpponentThrewPaper(game.Last());
+      case "C": return OpponentThrewScissors(game.Last());
+      default:  return 0;
+    }
+  }
+
+  private static int OpponentThrewRock(string ours)
+  {
+    switch (ours) 
+    {
+      case "X": return 3;
+      case "Y": return 6;
+      case "Z": return 0;
+      default:  return 0;
+    }
+  }
+
+  private static int OpponentThrewPaper(string ours)
+  {
+    switch (ours) 
+    {
+      case "X": return 0;
+      case "Y": return 3;
+      case "Z": return 6;
+      default:  return 0;
+    }
+  }
+
+  private static int OpponentThrewScissors(string ours)
+  {
+    switch (ours) 
+    {
+      case "X": return 6;
+      case "Y": return 0;
+      case "Z": return 3;
+      default:  return 0;
     }
   }
 
