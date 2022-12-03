4c63c05670898656ff8794ae6a5ea0f5ab14b57f
mcbtay <mcbtay@gmail.com>
2022-12-03 18:49:23 -0500

    adding stub Day3 file

---
+++ 2022/Day3/Day3.cs
@@ -0,0 +1,10 @@
+// https://adventofcode.com/2022/day/3
+// --- Day 3: Rucksack Reorganization ---
+
+public class Day3
+{
+    public static void RucksackReorganization()
+    {
+        Console.WriteLine(" --- Day 3 ---");
+    }
+}
