e1d5564a927c2c0cf07eff31013ee7b96d6a12db
Bryan Taylor <mcbtay@gmail.com>
2022-12-02 23:53:12 -0500

    moving to day1 folder and updating to read input from file

--- 2022/Day1/Day1.cs
+++ 2022/Day1/Day1.cs
@@ -3,25 +3,11 @@
 
 public class Day1 
 {
-  private static string exampleInput = 
-@"1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
-
   static void Main()
   {
-    var elves = ParseInput(exampleInput);
+    var elves = ParseInput();
+
+    Console.WriteLine($"{elves.Count} elves...");
 
     var highestCalories = 0;
 
@@ -36,14 +22,12 @@ public class Day1
     Console.WriteLine($"Elf with most calories has {highestCalories} calories.");
   }
 
-  private static List<List<int>> ParseInput(string input)
+  private static List<List<int>> ParseInput()
   {
-    var lines = exampleInput.Split(Environment.NewLine);
-
     var elves = new List<List<int>>();
     var currentElf = new List<int>();
     
-    foreach (var line in lines) 
+    foreach (var line in System.IO.File.ReadLines(@"Day1/example_input.txt")) 
     {
       if (string.IsNullOrEmpty(line.Trim())) 
       {
@@ -54,9 +38,9 @@ public class Day1
 
       if (!Int32.TryParse(line, out var lineInt)) continue;
       currentElf.Add(lineInt);
-
-      if (line == lines.Last()) elves.Add(currentElf);
     }
+
+    elves.Add(currentElf);
 
     return elves;
   }
