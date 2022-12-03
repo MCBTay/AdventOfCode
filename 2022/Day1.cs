4da43c5a062bd0431ee9765cc7dde73848ff07eb
Bryan Taylor <mcbtay@gmail.com>
2022-12-02 23:41:06 -0500

    adding method to parse input from prompt

--- 2022/Day1.cs
+++ 2022/Day1.cs
@@ -21,6 +21,31 @@ public class Day1
 
   static void Main()
   {
-    Console.Write(exampleInput);
+    var elves = ParseInput(exampleInput);
+  }
+
+  private static List<List<int>> ParseInput(string input)
+  {
+    var lines = exampleInput.Split(Environment.NewLine);
+
+    var elves = new List<List<int>>();
+    var currentElf = new List<int>();
+    
+    foreach (var line in lines) 
+    {
+      if (string.IsNullOrEmpty(line.Trim())) 
+      {
+        elves.Add(currentElf);
+        currentElf = new List<int>();
+        continue;
+      }
+
+      if (!Int32.TryParse(line, out var lineInt)) continue;
+      currentElf.Add(lineInt);
+
+      if (line == lines.Last()) elves.Add(currentElf);
+    }
+
+    return elves;
   }
 }
