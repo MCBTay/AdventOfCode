4fd85519f28387f07fcb7e4f8e979ae9ba4c73b3
Bryan Taylor <mcbtay@gmail.com>
2022-12-03 23:59:06 -0500

    stubbing out day4 files

---
+++ 2022/Day4/Day4.cs
@@ -0,0 +1,26 @@
+// https://adventofcode.com/2022/day/4
+// --- Day 4: Rucksack Reorganization ---
+
+public class Day4
+{
+    public static void RucksackReorganization()
+    {
+        Console.WriteLine(" --- Day 4 ---");
+        var rucksacks = ParseInput();
+    }
+
+    private static List<List<string>> ParseInput()
+    {
+        var rucksacks = new List<List<string>>();
+    
+        foreach (var line in System.IO.File.ReadLines(@"Day4/sample_input.txt")) 
+        {
+            var firstCompartment = String.Concat(line.Take(line.Length / 2));
+            var secondCompartment = String.Concat(line.TakeLast(line.Length / 2));
+
+            rucksacks.Add(new List<string> { firstCompartment, secondCompartment });
+        }
+
+        return rucksacks;
+    }
+}
