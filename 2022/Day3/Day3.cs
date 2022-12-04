be7a208ffa14c2f0e21fef5f97bee31848666eae
mcbtay <mcbtay@gmail.com>
2022-12-03 19:09:51 -0500

    logic to parse input and find the comon item between each compartment in the rucksack

--- 2022/Day3/Day3.cs
+++ 2022/Day3/Day3.cs
@@ -6,5 +6,45 @@ public class Day3
     public static void RucksackReorganization()
     {
         Console.WriteLine(" --- Day 3 ---");
+        var rucksacks = ParseInput();
+
+        var sumOfPriorities = 0;
+        foreach (var rucksack in rucksacks)
+        {
+            var commonItem = FindCommonItem(rucksack);
+            Console.WriteLine(commonItem);
+        }
+        Console.WriteLine($"Sum of the priorities of items in each compartment of all rucksacks: {sumOfPriorities}");
+    }
+
+    private static string FindCommonItem(List<string> rucksack)
+    {
+        var firstCompartment = rucksack.First();
+        var secondCompartment = rucksack.Last();
+
+        foreach (var item in firstCompartment)
+        {
+            foreach (var item2 in secondCompartment)
+            {
+                if (item == item2) return item.ToString();
+            }
+        }
+
+        return string.Empty;
+    }
+
+    private static List<List<string>> ParseInput()
+    {
+        var rucksacks = new List<List<string>>();
+    
+        foreach (var line in System.IO.File.ReadLines(@"Day3/sample_input.txt")) 
+        {
+            var firstCompartment = String.Concat(line.Take(line.Length / 2));
+            var secondCompartment = String.Concat(line.TakeLast(line.Length / 2));
+
+            rucksacks.Add(new List<string> { firstCompartment, secondCompartment });
+        }
+
+        return rucksacks;
     }
 }
