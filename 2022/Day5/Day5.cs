0e7207614172b5b5703312b20ee25e424faabb5f
Bryan Taylor <mcbtay@gmail.com>
2022-12-06 00:01:38 -0500

    parsing input

--- 2022/Day5/Day5.cs
+++ 2022/Day5/Day5.cs
@@ -1,25 +1,87 @@
 // https://adventofcode.com/2022/day/5
-// --- Day 4: Supply Stacks ---
+// --- Day 5: Supply Stacks ---
 
 public class Day5
 {
+    private static string filename = @"Day5/sample_input.txt";
     public static void SupplyStacks()
     {
-        Console.WriteLine(" --- Day 5---");
-        var assignments = ParseInput();
+      Console.WriteLine(" --- Day 5---");
+      var stacks = ParseStacksInput();
+      var moves = ParseMovesInput();
+
+      for (int i = 0; i < stacks.Count(); i++)
+      {
+        Console.Write($"Stack {i}: ");
+        foreach (var crate in stacks[i])
+        {
+          Console.Write(crate + ", ");
+        }
+        Console.WriteLine();
+      }
     }
 
-    private static List<List<List<int>>> ParseInput()
+    private static List<List<string>> ParseStacksInput()
     {
-        var assignments = new List<List<List<int>>>();
-    
-        foreach (var line in System.IO.File.ReadLines(@"Day4/input.txt")) 
-        {
-          var pair = new List<List<int>>();
+      var stacks = new List<List<string>>();
 
-          assignments.Add(pair);
+      var numStacks = 0;
+    
+      foreach (var line in System.IO.File.ReadLines(filename)) 
+      {
+        if (string.IsNullOrEmpty(line)) break;
+        if (!line.Contains('[')) continue;
+
+        if (numStacks == 0)
+        {
+          numStacks = (line.Length + 1) / 4;
+
+          for (int i = 0; i < numStacks; i++)
+          {
+            stacks.Add(new List<string>());
+          }
         }
 
-        return assignments;
+        for (int i = 0; i < line.Length; i++)
+        {
+          if (line[i] == '[') continue;
+          if (line[i] == ']') continue;
+          if (line[i] == ' ') continue;
+
+          var stackIndex = i / 3;
+          if (stackIndex == stacks.Count()) stackIndex -= 1;
+          stacks[stackIndex].Insert(0, line[i].ToString());
+        }
+      }
+
+      return stacks;
+    }
+
+    private static List<List<int>> ParseMovesInput()
+    {
+        var moves = new List<List<int>>();
+        
+        var parseMoves = false;
+    
+        foreach (var line in System.IO.File.ReadLines(filename)) 
+        {
+          if (string.IsNullOrEmpty(line)) {
+            parseMoves = true;
+            continue;
+          }
+
+          if (parseMoves)
+          {
+            var split = line.Split(' ');
+            moves.Add(new List<int> 
+            { 
+              Convert.ToInt32(split[1]),
+              Convert.ToInt32(split[3]),
+              Convert.ToInt32(split[5]),
+            });
+          }
+        }
+
+        return moves;
     }
 }
