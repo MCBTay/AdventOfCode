b22144397ce5d8c8619bf95ea7c0f61013d9ec97
mcbtay <mcbtay@gmail.com>
2022-12-11 10:51:35 -0500

    stubbing out day7 files

---
+++ 2022/Day7/Day7.cs
@@ -0,0 +1,12 @@
+// https://adventofcode.com/2022/day/7
+// --- Day 6: No Space Left On Device ---
+
+public class Day7
+{
+    private static string filename = @"Day7/sample_input.txt";
+    public static void NoSpaceLeftOnDevice()
+    {
+      Console.WriteLine(" --- Day 7---");
+
+    }
+}
