d7450f4ff134354c2ace56ab27aaf45652ce2f79
mcbtay <mcbtay@gmail.com>
2022-12-11 13:20:45 -0500

    part one solved

--- 2022/Day8/Day8.cs
+++ 2022/Day8/Day8.cs
@@ -1,9 +1,10 @@
+using System.Linq;
 // https://adventofcode.com/2022/day/8
 // --- Day 8: Treetop Tree House ---
 
 public class Day8
 {
-    private static string filename = @"Day8/sample_input.txt";
+    private static string filename = @"Day8/input.txt";
     
     public static void TreetopTreeHouse()
     {
@@ -11,14 +12,39 @@ public class Day8
 
       var trees = ParseInput();
       
-      foreach (var row in trees)
+      Console.WriteLine($"There are {GetVisibleTrees(trees)} visible trees.");
+    }
+
+    private static int GetVisibleTrees(List<List<int>> treeGrid)
+    {
+      var exteriorTrees = (treeGrid.Count() * 2) + ((treeGrid.First().Count() - 2) * 2);
+
+      var interiorTrees = 0;
+
+      for (int i = 1; i < treeGrid.Count() - 1; i++)
       {
-        foreach (var tree in row)
+        for (int j = 1; j < treeGrid[i].Count() - 1; j++)
         {
-          Console.Write(tree);
+          if (VisibleInRow(treeGrid[i], j)) interiorTrees++;
+          else if (VisibleInColumn(treeGrid, i, j)) interiorTrees++;
         }
-        Console.WriteLine();
       }
+
+      return exteriorTrees + interiorTrees;
+    }
+
+    private static bool VisibleInRow(List<int> treeRow, int j)
+    {      
+      return !treeRow.Take(j).Any(x => x >= treeRow[j]) // visible from left
+        || !treeRow.TakeLast(treeRow.Count() - (j + 1)).Any(x => x >= treeRow[j]); // visible from right
+    }
+
+    private static bool VisibleInColumn(List<List<int>> treeGrid, int i, int j)
+    {
+      var column = treeGrid.Select(treeRow => treeRow[j]).ToList();
+      
+      return !column.Take(i).Any(x => x >= column[i]) // visible from top
+        || !column.TakeLast(column.Count() - (i + 1)).Any(x => x >= column[i]); // visible from bottom
     }
 
     private static List<List<int>> ParseInput()
