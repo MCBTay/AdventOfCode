b39e88b3db779f046a187dbbd54e9edf17064cf3
mcbtay <mcbtay@gmail.com>
2022-12-11 13:46:45 -0500

    part 2 solved

--- 2022/Day8/Day8.cs
+++ 2022/Day8/Day8.cs
@@ -21,16 +21,77 @@ public class Day8
 
       var interiorTrees = 0;
 
+      var scenicScore = 0;
+
       for (int i = 1; i < treeGrid.Count() - 1; i++)
       {
         for (int j = 1; j < treeGrid[i].Count() - 1; j++)
         {
           if (VisibleInRow(treeGrid[i], j)) interiorTrees++;
           else if (VisibleInColumn(treeGrid, i, j)) interiorTrees++;
+
+          if (GetScenicScore(treeGrid, i, j) > scenicScore)
+          {
+            scenicScore = GetScenicScore(treeGrid, i, j);
+          }
         }
       }
 
+      Console.WriteLine($"Max scenic score is: {scenicScore}");
+
       return exteriorTrees + interiorTrees;
+    }
+
+    private static int GetScenicScore(List<List<int>> treeGrid, int i, int j)
+    {
+      return VisibleLeft(treeGrid, i, j) 
+        * VisibleRight(treeGrid, i, j)
+        * VisibleTop(treeGrid, i, j)
+        * VisibleBottom(treeGrid, i, j);
+    }
+
+    private static int VisibleLeft(List<List<int>> treeGrid, int i, int j)
+    { 
+      var treesLookingLeft = treeGrid[i].Take(j).Reverse().ToList();
+
+      for (int x = 0; x < treesLookingLeft.Count(); x++)
+      {
+        if (treesLookingLeft[x] >= treeGrid[i][j]) return x + 1;
+      }
+      return treesLookingLeft.Count();
+    }
+    
+    private static int VisibleRight(List<List<int>> treeGrid, int i, int j) 
+    { 
+      var treesLookingRight = treeGrid[i].TakeLast(treeGrid[i].Count() - (j + 1)).ToList();
+
+      for (int x = 0; x < treesLookingRight.Count(); x++)
+      {
+        if (treesLookingRight[x] >= treeGrid[i][j]) return x + 1;
+      }
+      return treesLookingRight.Count();
+    }
+
+    private static int VisibleTop(List<List<int>> treeGrid, int i, int j) 
+    { 
+      var treesLookingUp = treeGrid.Select(treeRow => treeRow[j]).Take(i).Reverse().ToList();
+
+      for (int x = 0; x < treesLookingUp.Count(); x++)
+      {
+        if (treesLookingUp[x] >= treeGrid[i][j]) return x + 1;
+      }
+      return treesLookingUp.Count();
+    }
+
+    private static int VisibleBottom(List<List<int>> treeGrid, int i, int j) 
+    { 
+      var treesLookingDown = treeGrid.Select(treeRow => treeRow[j]).TakeLast(treeGrid.Select(treeRow => treeRow[j]).Count() - (i + 1)).ToList();
+
+      for (int x = 0; x < treesLookingDown.Count(); x++)
+      {
+        if (treesLookingDown[x] >= treeGrid[i][j]) return x + 1;
+      }
+      return treesLookingDown.Count();
     }
 
     private static bool VisibleInRow(List<int> treeRow, int j)
