de3f07f4ed51f0a8754a7da96b54c3d9ddfb5aff
mcbtay <mcbtay@gmail.com>
2022-12-11 12:39:58 -0500

    parsing tree grid

--- 2022/Day8/Day8.cs
+++ 2022/Day8/Day8.cs
@@ -8,5 +8,35 @@ public class Day8
     public static void TreetopTreeHouse()
     {
       Console.WriteLine(" --- Day 8 ---");
+
+      var trees = ParseInput();
+      
+      foreach (var row in trees)
+      {
+        foreach (var tree in row)
+        {
+          Console.Write(tree);
+        }
+        Console.WriteLine();
+      }
+    }
+
+    private static List<List<int>> ParseInput()
+    {
+      var treeGrid = new List<List<int>>();
+      var treeRow = new List<int>();
+      
+      foreach (var line in System.IO.File.ReadLines(filename)) 
+      {
+        foreach (var character in line)
+        {
+          if (!int.TryParse(character.ToString(), out var treeHeight)) continue;
+          treeRow.Add(treeHeight);          
+        }
+        treeGrid.Add(treeRow);
+        treeRow = new List<int>();
+      }
+
+      return treeGrid;
     }
 }
