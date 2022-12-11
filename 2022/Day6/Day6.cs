09647e9f9e9cd6996831b9998abdc84658599a15
mcbtay <mcbtay@gmail.com>
2022-12-11 10:23:58 -0500

    stubbing out day 6 files

---
+++ 2022/Day6/Day6.cs
@@ -0,0 +1,12 @@
+// https://adventofcode.com/2022/day/5
+// --- Day 5: Supply Stacks ---
+
+public class Day6
+{
+    private static string filename = @"Day6/sample_input.txt";
+    public static void TuningTrouble()
+    {
+      Console.WriteLine(" --- Day 6---");
+      
+    }
+}
