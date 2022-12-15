282d4fa8f02a4fe4ada90db5d48be3a844029aca
Bryan Taylor <mcbtay@gmail.com>
2022-12-14 23:21:46 -0500

    stubbing day 11 files

---
+++ 2022/Day11/Day11.cs
@@ -0,0 +1,13 @@
+using System.Linq;
+// https://adventofcode.com/2022/day/11
+// --- Day 11: Monkey in the Middle ---
+
+public class Day11
+{
+    private static string filename = @"Day11/sample_input.txt";
+
+    public static void MonkeyInTheMiddle()
+    {
+      Console.WriteLine(" --- Day 11 ---");
+    }
+}
