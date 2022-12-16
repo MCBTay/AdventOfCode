2adecf84eaba079b9e560af8abb959ee159bfd54
Bryan Taylor <mcbtay@gmail.com>
2022-12-16 18:22:14 -0500

    current progress

--- 2022/Day12/Day12.cs
+++ 2022/Day12/Day12.cs
@@ -3,12 +3,19 @@
 
 public class Day12
 {
-    private static string filename = @"Day12/sample_input.txt";
+    private static string filename = @"Day12/input.txt";
 
     private static List<List<char>> HeightMap;
     private static Index StartingIndex;
     private static Index EndingIndex;
+    
     private static int StepCount;
+    private static Index CurrentIndex;
+
+    private static List<Index> Steps = new List<Index>();
+    private static List<Index> FailedSteps = new List<Index>();
+
+    private static char CurrentValue => HeightMap[CurrentIndex.Row][CurrentIndex.Col];
 
     public static void HillClimbingAlgorithm()
     {
@@ -17,6 +24,77 @@ public class Day12
       HeightMap = ParseInput();
       PrintHeightMap();
       SetIndices();
+
+      CurrentIndex = StartingIndex;
+      Steps.Add(CurrentIndex);
+      DeterminePath();
+
+      Console.WriteLine($"Step count is {Steps.Count}");
+    }
+
+    private static void DeterminePath()
+    {
+      var indeces = GetIndecesToConsider(CurrentIndex).OrderBy(x => CurrentValue.CompareTo(HeightMap[x.Row][x.Col]));
+
+      foreach(var index in indeces)
+      {
+        if (FailedSteps.Contains(index)) continue;
+
+        var testValue = HeightMap[index.Row][index.Col];
+
+        if (CurrentValue == 'S' && testValue == 'a')
+        {
+          UpdateCurrentIndex(index);
+          return;
+        }
+        else if (CurrentValue.CompareTo(testValue) == -1)
+        {
+          UpdateCurrentIndex(index);
+          return;
+        }
+        else if (CurrentValue.CompareTo(testValue) == 0 && !Steps.Contains(index))
+        {
+          UpdateCurrentIndex(index);
+          return;
+        }
+        else if (CurrentValue == 'z' && testValue == 'E')
+        {
+          Console.WriteLine("woohoo");
+          return;
+        }
+      }
+
+      FailedSteps.Add(new Index(CurrentIndex));
+      Steps.RemoveAt(Steps.Count - 1);
+      CurrentIndex = new Index(Steps.Last());
+      DeterminePath();
+    }
+
+    private static void UpdateCurrentIndex(Index index)
+    {
+      CurrentIndex = new Index(index);
+      Steps.Add(CurrentIndex);
+      Console.WriteLine($"Current index is {CurrentIndex}");
+      DeterminePath();
+    }
+
+    private static List<Index> GetIndecesToConsider(Index index)
+    {
+      var indeces = new List<Index>()
+      {
+        new Index(index.Row - 1, index.Col),
+        new Index(index.Row + 1, index.Col),
+        new Index(index.Row, index.Col - 1),
+        new Index(index.Row, index.Col + 1)
+      };
+
+      indeces.RemoveAll(
+        x => x.Row < 0 
+        || x.Row > HeightMap.Count  - 1
+        || x.Col < 0 
+        || x.Col > HeightMap[0].Count - 1);
+    
+      return indeces;
     }
 
     private static void SetIndices()
@@ -76,6 +154,25 @@ public class Day12
       {
         Row = row;
         Col = col;
+      }
+
+      public Index(Index index) : this(index.Row, index.Col)
+      {
+
+      }
+
+      public override string ToString()
+      {
+        return $"({Row}, {Col})[{HeightMap[Row][Col]}]";
+      }
+
+      public override bool Equals(object obj) => this.Equals(obj as Index);
+
+      public bool Equals(Index other)
+      {
+        if (other is null) return false;
+        if (Object.ReferenceEquals(this, other)) return true;
+        return this.Row == other.Row && this.Col == other.Col;
       }
     }
 }
