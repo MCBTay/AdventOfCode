bf6f19ada4ebcfccd5659e06050e7d630b22d33a
mcbtay <mcbtay@gmail.com>
2023-12-02 12:34:51 -0500

    solved for part 2

--- 2023/Day2/Day2.cs
+++ 2023/Day2/Day2.cs
@@ -9,15 +9,19 @@ public class Day2
     var games = ParseInput();
 
     var sum = 0;
+    var sumOfPowers = 0;
     foreach (var game in games)
     {      
       if (game.CubeSets.All(x => x.IsPossible()))
       {
         sum += game.Id;
       }
+
+      sumOfPowers += game.GetPowerOfSet();
     }
 
     Console.WriteLine($"Sum of game IDs is {sum}.");
+    Console.WriteLine($"Sum of powers is {sumOfPowers}.");
   }
 
   private static List<Game> ParseInput()
@@ -26,51 +30,70 @@ public class Day2
     
     foreach (var line in System.IO.File.ReadLines(@"Day2/input.txt")) 
     {
-        var game = new Game();
-
-        var split = line.Split(":");
-        game.Id = Int32.Parse(split[0].Split(" ")[1]);
-
-        var cubeSets = split[1].Split(';');
-
-        foreach (var cubeSet in cubeSets)
-        {
-          var newCubeSet = new CubeSet();
-
-          var cubes = cubeSet.Split(',');
-
-          foreach (var cube in cubes)
-          {
-            var numberAndColor = cube.Trim().Split(' ');
-
-            switch (numberAndColor[1])
-            {
-              case "blue": 
-                newCubeSet.BlueCubes = Int32.Parse(numberAndColor[0]);
-                break;
-              case "green":
-                newCubeSet.GreenCubes = Int32.Parse(numberAndColor[0]);
-                break;
-              case "red":
-                newCubeSet.RedCubes = Int32.Parse(numberAndColor[0]);
-                break;
-              default: break;
-            }
-          }
-
-          game.CubeSets.Add(newCubeSet);
-        }
-
+        var game = ParseGame(line);
         games.Add(game);
     }
 
     return games;
   }
 
+  private static Game ParseGame(string line)
+  {
+    var game = new Game();
+
+    var split = line.Split(":");
+    game.Id = Int32.Parse(split[0].Split(" ")[1]);
+
+    var cubeSets = split[1].Split(';');
+
+    foreach (var cubeSet in cubeSets)
+    {
+      var newCubeSet = ParseCubeSet(cubeSet);
+
+      game.CubeSets.Add(newCubeSet);
+    }
+
+    return game;
+  }
+
+  private static CubeSet ParseCubeSet(string input)
+  {
+    var cubeSet = new CubeSet();
+
+    var cubes = input.Split(',');
+
+    foreach (var cube in cubes)
+    {
+      var numberAndColor = cube.Trim().Split(' ');
+
+      switch (numberAndColor[1])
+      {
+        case "blue": 
+          cubeSet.BlueCubes = Int32.Parse(numberAndColor[0]);
+          break;
+        case "green":
+          cubeSet.GreenCubes = Int32.Parse(numberAndColor[0]);
+          break;
+        case "red":
+          cubeSet.RedCubes = Int32.Parse(numberAndColor[0]);
+          break;
+        default: break;
+      }
+    }
+
+    return cubeSet;
+  }
+
   public class Game 
   {
     public int Id { get; set; }
-    public List<CubeSet> CubeSets { get; set; } = new List<CubeSet>(); 
+    public List<CubeSet> CubeSets { get; set; } = new List<CubeSet>();
+
+    public int GetPowerOfSet() => GetMinimunNumberOfRedCubes() * GetMinimunNumberOfGreenCubes() * GetMinimunNumberOfBlueCubes();
+
+    private int GetMinimunNumberOfRedCubes() => CubeSets.Select(x => x.RedCubes).Max();
+    private int GetMinimunNumberOfBlueCubes() => CubeSets.Select(x => x.BlueCubes).Max();
+    private int GetMinimunNumberOfGreenCubes() => CubeSets.Select(x => x.GreenCubes).Max();
   }
 
   public class CubeSet 
@@ -79,9 +102,6 @@ public class Day2
     public int BlueCubes { get; set; }
     public int GreenCubes { get; set; }
 
-    public bool IsPossible()
-    {
-      return RedCubes <= 12 && GreenCubes <= 13 && BlueCubes <= 14;
-    }
+    public bool IsPossible() => RedCubes <= 12 && GreenCubes <= 13 && BlueCubes <= 14;
   }
 }
