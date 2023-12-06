15faf93958c9c8c822d0775b67a5ec82c8b1e155
mcbtay <mcbtay@gmail.com>
2023-12-05 23:24:43 -0500

    committing progress

--- 2023/Day5/Day5.cs
+++ 2023/Day5/Day5.cs
@@ -21,6 +21,7 @@ public class Day5
         ParseInput();
 
         GetLowestLocation();
+        GetLowestLocationSeedPairs();
     }
 
     private static void GetLowestLocation()
@@ -28,18 +29,38 @@ public class Day5
         Console.WriteLine($"Lowest location is {almanac.Seeds.Select(x => x.GetLocation()).Min()}.");
     }
 
+    private static void GetLowestLocationSeedPairs()
+    {
+        var lowest = almanac.SeedPairs
+            .Select(x => (seed: x, range: almanac.SeedToSoilMap.SliceRanges(x.Range)))
+            .Select(x => (seed: x.seed, range: almanac.SoilToFertilizerMap.SliceRanges(x.range)))
+            .Select(x => (seed: x.seed, range: almanac.FertilizerToWaterMap.SliceRanges(x.range)))
+            .Select(x => (seed: x.seed, range: almanac.WaterToLightMap.SliceRanges(x.range)))
+            .Select(x => (seed: x.seed, range: almanac.LightToTemperatureMap.SliceRanges(x.range)))
+            .Select(x => (seed: x.seed, range: almanac.TemperatureToHumidityMap.SliceRanges(x.range)))
+            .Select(x => (seed: x.seed, range: almanac.HumidityToLocationMap.SliceRanges(x.range)))
+            .SelectMany(x =>
+                x.range.GetStartIndexes().OrderBy(y => y.Key).Select(y =>
+                    (seed: x.seed.Range.Intervals[0].Start + y.Value, location: y.Key)))
+            .OrderBy(x => x.location)
+            .First();
+
+        Console.WriteLine($"Lowest location (seed pairs) is {lowest.location}.");
+    }
+
     private static void ParseInput()
     {
-        var text = File.ReadAllText(@"Day5/input.txt");
+        var text = File.ReadAllText(@"Day5/example_input.txt");
 
         var lines = text.Split('\n').ToList();
 
-        List<MapEntry>? mapToUpdate = null;
+        Map? mapToUpdate = null;
         foreach (var line in lines)
         {
             if (line.Contains("seeds:"))
             {
                 ParseSeeds(line);
+                ParseSeedPairs(line);
                 continue;
             }
 
@@ -57,7 +78,7 @@ public class Day5
 
                 var mapEntry = new MapEntry(entries.ElementAt(0), entries.ElementAt(1), entries.ElementAt(2));
 
-                mapToUpdate.Add(mapEntry);
+                mapToUpdate.Entries.Add(mapEntry);
             }
             else
             {
@@ -76,7 +97,15 @@ public class Day5
           .ToList();
     }
 
-    private static List<MapEntry> DetermineMapToUpdate(string line)
+    private static void ParseSeedPairs(string line)
+    {
+        for (int i = 0; i < almanac.Seeds.Count; i += 2)
+        {
+            almanac.SeedPairs.Add(new SeedPair(almanac.Seeds.ElementAt(i).Id, almanac.Seeds.ElementAt(i + 1).Id));
+        }
+    }
+
+    private static Map DetermineMapToUpdate(string line)
     {
         return line switch
         {
@@ -94,14 +123,87 @@ public class Day5
     public class Almanac
     {
         public List<Seed> Seeds { get; set; } = new List<Seed>();
+        public List<SeedPair> SeedPairs { get; set; } = new List<SeedPair>();
 
-        public List<MapEntry> SeedToSoilMap { get; set; } = new List<MapEntry>();
-        public List<MapEntry> SoilToFertilizerMap { get; set; } = new List<MapEntry>();
-        public List<MapEntry> FertilizerToWaterMap { get; set; } = new List<MapEntry>();
-        public List<MapEntry> WaterToLightMap { get; set; } = new List<MapEntry>();
-        public List<MapEntry> LightToTemperatureMap { get; set; } = new List<MapEntry>();
-        public List<MapEntry> TemperatureToHumidityMap { get; set; } = new List<MapEntry>();
-        public List<MapEntry> HumidityToLocationMap { get; set; } = new List<MapEntry>();
+        public Map SeedToSoilMap { get; set; } = new Map();
+        public Map SoilToFertilizerMap { get; set; } = new Map();
+        public Map FertilizerToWaterMap { get; set; } = new Map();
+        public Map WaterToLightMap { get; set; } = new Map();
+        public Map LightToTemperatureMap { get; set; } = new Map();
+        public Map TemperatureToHumidityMap { get; set; } = new Map();
+        public Map HumidityToLocationMap { get; set; } = new Map();
+    }
+
+    public class Map
+    {
+        public List<MapEntry> Entries { get; set; } = new List<MapEntry>();
+
+        public Range SliceRanges(Range range)
+        {
+            var currentRange = range;
+
+            foreach (var mapping in Entries)
+            {
+                currentRange = mapping.SliceRange(currentRange);
+            }
+
+            foreach (var interval in currentRange.Intervals)
+            {
+                interval.Start = MapLookup(interval.Start);
+            }
+
+            return currentRange;
+        }
+    }
+
+    public class Interval
+    {
+        public long Start { get; set; }
+        public long Length { get; set; }
+        public long End => Start + Length;
+
+        public Interval(long start, long length)
+        {
+            Start = start;
+            Length = length;
+        }
+    }
+
+    public class Range
+    {
+        public List<Interval> Intervals { get; set; } = new List<Interval>();
+
+        public Range() : this(0, 0) { }
+
+        public Range(long start, long length)
+        {
+            Intervals.Add(new Interval(start, length));
+        }
+
+        public Dictionary<long, long> GetStartIndexes()
+        {
+            var startIndexes = new Dictionary<long, long>();
+            
+            long currentIndex = 0; 
+
+            foreach (var interval in Intervals)
+            {
+                startIndexes[interval.Start] = currentIndex;
+                currentIndex += interval.Length;
+            }
+
+            return startIndexes;
+        }
+    }
+
+    public class SeedPair : Seed
+    {
+        public Range Range { get; set; }
+
+        public SeedPair(long start, long length) : base(0)
+        {
+            Range = new Range(start, length);
+        }
     }
 
     public class Seed
@@ -124,11 +226,11 @@ public class Day5
             return MapLookup(almanac.HumidityToLocationMap, humd);
         }
 
-        private long MapLookup(List<MapEntry> map, long id)
+        protected long MapLookup(Map map, long id)
         {
-            var answer = map //almanac.SeedToSoilMap
-                .Where(x => id >= x.SourceRangeStart && id <= x.SourceRangeStart + x.RangeLength)
-                .Select(x => x.DestRangeStart + (id - x.SourceRangeStart))
+            var answer = map.Entries
+                .Where(x => id >= x.Source.Start && id <= x.Source.End)
+                .Select(x => x.Dest.Start + (id - x.Source.Start))
                 .FirstOrDefault();
 
             if (answer == 0) answer = id;
@@ -139,15 +241,49 @@ public class Day5
 
     public class MapEntry
     {
-        public long DestRangeStart { get; set; }
-        public long SourceRangeStart { get; set; }
-        public long RangeLength { get; set; }
+        public Interval Dest { get; set; }
+        public Interval Source { get; set; }
 
         public MapEntry(long destRangeStart, long sourceRangeStart, long rangeLength)
         {
-            DestRangeStart = destRangeStart;
-            SourceRangeStart = sourceRangeStart;
-            RangeLength = rangeLength;
+            Dest = new Interval(destRangeStart, rangeLength);
+            Source = new Interval(sourceRangeStart, rangeLength);
+        }
+
+        public Range SliceRange(Range range)
+        {
+            var newRange = new Range();
+
+            foreach (var interval in range.Intervals)
+            {
+                if (interval.End <= Source.Start || interval.Start >= Source.End)
+                {
+                    // interval doesn't overlap with this entry
+                    newRange.Intervals.Add(interval);
+                }
+                else
+                {
+                    // add part of the interval before this entry
+                    if (interval.Start < Source.Start)
+                    {
+                        newRange.Intervals.Add(new Interval(interval.Start, Source.Start - interval.Start));
+                    }
+
+                    // add the part of the interval after this entry
+                    if (interval.End > Source.End)
+                    {
+                        newRange.Intervals.Add(new Interval(Source.End, interval.End - Source.End));
+                    }
+
+                    // now the parts that overlap
+                    long overlapStart = Math.Max(interval.Start, Source.Start);
+                    long overlapEnd = Math.Min(interval.End, Source.End);
+
+                    newRange.Intervals.Add(new Interval(overlapStart, overlapEnd - overlapStart));
+                }
+            }
+
+            return newRange;
         }
     }
 }
